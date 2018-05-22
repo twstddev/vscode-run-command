d0b4bf186490cf288cf36008e132078a563055e7
Rustam Chakin <rustam.chakin@paysafe.com>
2018-05-22 13:06:37 +0100

    Add simple implementation that reads custom mappings from configuration and registers appropriate commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,18 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    const configuration = vscode.workspace.getConfiguration();
+    const mappings = configuration.get('vscode.run.commands.mappings', []);
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-run-command" is now active!');
+    mappings.forEach((mapping: any) => {
+        const handler = vscode.commands.registerCommand(mapping.from, () => {
+            vscode.commands.executeCommand(mapping.to, mappings.args);
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+            return Promise.resolve();
+        });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        context.subscriptions.push(handler);
     });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
